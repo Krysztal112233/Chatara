e5e8891ac448f4f37e165f68f97d14d9e1029bc5
Krysztal Huang <krysztal.huang@outlook.com>
2025-09-23 22:21:17 +0800

    feat(backend): defined response structure `CommonResponse`

---
+++ backend/crates/charata/src/common/mod.rs
@@ -0,0 +1,55 @@
+use educe::Educe;
+use rocket::{
+    http::{ContentType, Status},
+    response::Responder,
+    serde::{json::Value, Deserialize, Serialize},
+    Response,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize, Educe)]
+#[educe(Default)]
+pub struct CommonResponse {
+    #[educe(Default = 200)]
+    code: u16,
+
+    #[educe(Default = "")]
+    #[serde(skip_serializing_if = "String::is_empty")]
+    msg: String,
+
+    #[serde(skip_serializing_if = "Value::is_null", flatten)]
+    data: Value,
+}
+
+#[allow(unused)]
+impl CommonResponse {
+    pub fn new(code: u16) -> Self {
+        Self {
+            code,
+            ..Default::default()
+        }
+    }
+
+    pub fn with_msg(code: u16, msg: String) -> Self {
+        Self {
+            code,
+            msg,
+            ..Default::default()
+        }
+    }
+
+    pub fn set_data(self, data: Value) -> Self {
+        Self { data, ..self }
+    }
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for CommonResponse {
+    fn respond_to(self, request: &'r rocket::Request<'_>) -> rocket::response::Result<'o> {
+        let status = Status::new(self.code);
+        let body = serde_json::to_string(&self).map_err(|_| Status::new(500))?;
+
+        Response::build_from(body.respond_to(request)?)
+            .status(status)
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
