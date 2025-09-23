2e4892dad0f90478f56a19bcc81176b69a84d8a7
Krysztal Huang <krysztal.huang@outlook.com>
2025-09-23 23:07:24 +0800

    feat(backend): enable logging

--- backend/crates/charata/src/main.rs
+++ backend/crates/charata/src/main.rs
@@ -1,3 +1,4 @@
+use log::LevelFilter;
 use mimalloc::MiMalloc;
 use rocket::Rocket;
 
@@ -13,5 +14,9 @@ mod error;
 
 #[rocket::launch]
 async fn rocket() -> _ {
+    let _ = env_logger::builder()
+        .filter_level(LevelFilter::Info)
+        .try_init();
+
     Rocket::build()
 }
